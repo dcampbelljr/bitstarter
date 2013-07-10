660e403e09a945c99259881d4e6689b4b35526a0
Don Campbell <don@impulz.net>
2013-07-10 19:15:24 -0400

    Update to read from file.

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  response.send(data.toString('utf-8'));
+  console.log(data.toString('utf-8'));
+});
 });
 
 var port = process.env.PORT || 5000;
